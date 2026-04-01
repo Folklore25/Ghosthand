b97d9eab1220ad3de92bcda74906190b01e28471
Zohar <rockboy1125@gmail.com>
2026-04-02 00:20:14 +0800

    chore(release): bump app version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.folklore25.ghosthand"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
